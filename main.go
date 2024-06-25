64d763d8d55e0ad443554e6330dcb732b7d69c19
priyansh-garg1 <priyanshgarg382@gmail.com>
2024-06-26 02:34:24 +0530

    Started Go

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+) 
+
+func main() {
+	fmt.Println("I am Learning Go Languages")
+
+	// // Variables
+	// var name string  = "Hello"
+	// var version = 1.001
+	// version = 2.001
+	// var dec float32 = 0.001
+	// fmt.Println(dec);/
+
+	person := 123
+	fmt.Println(person)
+}
