5cb4324047e2da8b4e351c4fea4c220267acec1f
priyansh-garg1 <priyanshgarg382@gmail.com>
2024-06-27 03:04:54 +0530

    Go Continue

--- main.go
+++ main.go
@@ -1,11 +1,9 @@
 package main
 
-import (
-	"fmt"
-) 
+import "fmt"
 
 func main() {
-	fmt.Println("I am Learning Go Languages")
+	// fmt.Println("I am Learning Go Languages")
 
 	// // Variables
 	// var name string  = "Hello"
@@ -14,6 +12,49 @@ func main() {
 	// var dec float32 = 0.001
 	// fmt.Println(dec);/
 
-	person := 123
-	fmt.Println(person)
+	// age := 12
+	// name := "User"
+	// person := 123
+	// var agee int = 12
+	// fmt.Println("person: ", person, "name: ", name, "age", age)
+	// fmt.Printf("age is %d", agee)
+	// fmt.Printf("name is %T", name)
+
+	// Input User
+	// fmt.Println("Enter your name")
+	// var name string
+	// Only Take first word
+	// fmt.Scan(&name)
+	// fmt.Printf("Hello %s", name)
+	// // to get full line
+	// reader := bufio.NewReader(os.Stdin)
+	// name, _ = reader.ReadString('\n')
+	// fmt.Println(name)
+
+	// simpleFunction()
+	// ans, err := div(5, 0)
+	// if err != nil {
+	// 	fmt.Println(err.Error())
+	// 	return
+	// }
+	ans, _ := div(5, 2)
+	fmt.Println("Division:", ans)
+}
+
+// // Function
+// func simpleFunction() {
+// 	fmt.Print("Function")
+// 	fmt.Println("val is:", add(10, 20))
+// }
+
+// func add(a, b int) int {
+// 	fmt.Println(a + b)
+// 	return a + b
+// }
+
+func div(a, b float32) (float32, error) {
+	if b == 0 {
+		return 0, fmt.Errorf("cannot divide by zero")
+	}
+	return a / b, nil
 }
